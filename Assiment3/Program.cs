92c3dcd083318980b12b63fc357ebeb1847aaa71
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-07-27 12:02:04 -0700

    Question2

--- Assiment3/Program.cs
+++ Assiment3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assiment3
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assiment3
 {
     internal class Program
     {
@@ -6,8 +8,19 @@
         {
             #region Question1
             //Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1).
-          /*  double[] prices = { 25.5, 40.0, 33.75 };
-                Console.WriteLine("The Second price : "+ prices[1]); */
+            /*  double[] prices = { 25.5, 40.0, 33.75 };
+                  Console.WriteLine("The Second price : "+ prices[1]); */
+            #endregion
+
+            #region Question2
+            //Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and  shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+           /* int[,] shelf =
+            {
+                {3,5},
+                {1,4}
+            };
+            Console.WriteLine("Print the number of copies on shelf 1, slot 0 = "+shelf[1,0]); */
+
             #endregion
         }
     }
